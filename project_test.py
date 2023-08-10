31d1b8aea6250f6b148618291253692cd56ebf15
szhang0706 <140440630+szhang0706@users.noreply.github.com>
2023-08-10 12:49:37 -0400

    Add files via upload

---
+++ project_test.py
@@ -0,0 +1,65 @@
+print ('''Weclome to CLUECODE. I will give you 6 questions, enter the
+correct anweser for each letter. are you ready? GO!''')
+anwser = None
+while anwser != "yangtze":
+    anwser = input("What is the 3rd longest river in the world?").lower()
+    if anwser != "yangtze":
+        print ("You are wrong, try again.")
+print ("correct. the first letter is U")
+anwser_2 = None
+while anwser_2 != "Salt":
+    anwser_2 = input("What substance does Sodium and Chloride make?").capitalize()
+    if anwser_2 != "Salt":
+        print ("You are wrong, try again.")
+print ("correct. you second letter is R")
+anwser_3 = None
+while anwser_3 != "4":
+    anwser_3 = input("How many terms did Franklin Delano Roosevelt Serve?")
+    if anwser_3 != "4":
+        print ("Your are wrong. try harder")
+print ("correct, your third letter is S")
+anwser_4 = None
+while anwser_4 != "6":
+    anwser_4 = input("What is the cube root of 216?")
+    if anwser_4 != "6":
+        print ("Your wrong. try harder")
+print ("correct your, fourth letter is M")
+anwser_5 = None
+while anwser_5 != "leo tigris":
+    anwser_5  = input("What is the scientific name for a tiger?").lower()
+    if anwser_5 != "leo tigris":
+        print ("Your wrong. try again.")
+print ("correct. your sixth is letter/word is ART")
+print ("add together all your letters to enter passcode.")
+passcode = None
+while passcode != "URSMART":
+    passcode = input("Enter your passcode: ")
+    if passcode != "URSMART":
+        print ("Invaild Passcode try again")
+print ("finally you got the correct passcode. Congratunlations. UR SMART")
+final_anwser =  None
+while final_anwser != "yes":
+    final_anwser =  input('''Would you like another challenge?''')
+    if final_anwser == "yes":
+        print ("Ok, you asked for it. Get a peice of paper")
+RIDDLE = None
+while RIDDLE != "Maid":
+    RIDDLE = input('''A wealthy family lived in a big circular house.
+They had a maid, a butler, and a gardener. The parents were going to a party
+, so they tucked the younger kids into bed and kissed them goodnight and said goodbye
+, and kissed the older kids goodnight. When the parents came home, 
+all the kids were gone—they had been kidnapped. The authorities ask the butler, maid, 
+and gardener what they were doing at the time of the kidnapping. The butler says 
+he was organizing the library, the maid says she was dusting the corners, and the 
+gardener says he was watering the plants. Who’s lying and actually kidnapped the kids?''').capitalize()
+    if RIDDLE != "Maid":
+        print("WRONG. TRY AGAIN.")
+print ("Ur a genius.")
+
+ 
+
+
+
+
+
+  
